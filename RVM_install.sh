3b77573c849b1c3b2a100d34024cecbe84a1e9ce
Strund3r <franzwagner.str@gmail.com>
2016-08-08 13:08:13 -0300

    first commit

---
+++ RVM_install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Install Ruby
+sudo apt-get update -y
+
+sudo apt-get install -y libgmp3-dev
+
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+# Install RVM
+sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+
+curl -L https://get.rvm.io | bash -s stable
+
+source ~/.rvm/scripts/rvm
+
+rvm install 2.3.1
+
+rvm use 2.3.1 --default
+
+ruby -v
+
+# Install Bundler Gem
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+
+gem install bundler
+
+bundler -v
+
+bundle install
+
+# Install NodeJS
+echo -ne '\n' | sudo add-apt-repository ppa:chris-lea/node.js
+
+sudo apt-get update
+
+sudo apt-get install -y nodejs
+
+# Install Rails
+gem install rails
+
+rails -v
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -d "~/myapp" ]; then
+  cd ~/myapp
+  rm -f ~/myapp/tmp/pids/server.pid
+  printf "\n\n"
+  cat message/server.txt
+  printf "\n\n"
+else
+  cd /home/franzwagner/Documents/ecs_teste
+  rm -f ~/Documents/ecs_teste/tmp/pids/server.pid
+  printf "\n\n"
+  cat message/localhost.txt
+  printf "\n\n"
+fi
+
+bundle exec rails server -b '0.0.0.0'
