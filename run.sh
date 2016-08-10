346457654d7124cb15e6c99ffe783c9b605a9504
Strund3r <franzwagner.str@gmail.com>
2016-08-10 16:47:32 -0300

    Removed cd commands and updated cat commands

--- run.sh
+++ run.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
 if [ -d "~/myapp" ]; then
-  cd ~/myapp
   rm -f ~/myapp/tmp/pids/server.pid
   printf "\n\n"
-  cat message/server.txt
+  cat ~/myapp/message/server.txt
   printf "\n\n"
 else
-  cd /home/franzwagner/Documents/ecs_teste
   rm -f ~/Documents/ecs_teste/tmp/pids/server.pid
-  rm -f /myapp/tmp/pids/server.pid
+  rm -f ~/myapp/tmp/pids/server.pid
   printf "\n\n"
-  cat message/localhost.txt
+  cat ~/Documents/ecs_teste/message/localhost.txt
   printf "\n\n"
 fi
 
