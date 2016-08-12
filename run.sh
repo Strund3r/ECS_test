bc2bf800823231fc8e2247e64ea62c8839b7d72f
Strund3r <franzwagner.str@gmail.com>
2016-08-12 13:27:47 -0300

    Updated run.sh with new RVM_install scripts

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 if [ -d "~/myapp" ]; then
+  ./RVM_install_server.sh
   rm -f ~/myapp/tmp/pids/server.pid
   printf "\n\n"
   cat ~/myapp/message/server.txt
   printf "\n\n"
 else
+  ./RVM_install_machine.sh
   rm -f ~/Documents/ecs_teste/tmp/pids/server.pid
   rm -f ~/myapp/tmp/pids/server.pid
   printf "\n\n"
   cat ~/Documents/ecs_teste/message/localhost.txt
   printf "\n\n"
 fi
-
-./RVM_install.sh
 
 bundle exec rails server -b '0.0.0.0'
