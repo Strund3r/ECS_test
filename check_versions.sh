e2cf96c8a863e7b3a3d4cb15e5acdbe5e275d2f6
Strund3r <franzwagner.str@gmail.com>
2016-08-19 13:38:22 -0300

    Updated check_version.sh

--- check_versions.sh
+++ check_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check versions
-printf "\n\n\n"
+printf "\n\n"
 
 echo -e "\t\t\t\e[1;4m* * * * * # Checking Versions # * * * * *\e[0m"
 
@@ -27,64 +27,74 @@ echo -ne '\t\t\t\e[1;4;38;5;226m##\e[1;4;38;5;56m###################\e[1;4;38;5;
 sleep 0.3
 
 echo -ne '\n\n\n'
-
-echo Docker version:
+################################################################################
+echo Docker Engine version:
 printf "\n"
-if [[ $docker_version = "Docker version 1.12.0, build 8eab29e" ]]; then
-  echo -e "\t\e[1;4;28;41m Docker 1.12.0 \e[0m"
+if [[ $docker_version = "Docker version 1.12.1, build 23cf638" ]]; then
+  echo -e "\t\e[1;4;28;41m Docker Engine 1.12.1 \e[0m"
 else
-  echo -e "\t\e[1;4;28;41m $docker_version \e[0m"
+  echo -e "\t\e[1;4;28;41m $docker_version \e[0m \t\e[1;4;31;40m* UPDATED *\e[0m"
 fi
 printf "\n"
 
 sleep 0.5
-
+################################################################################
 echo Docker Compose version:
 printf "\n"
 if [[ $docker_compose_version = "docker-compose version 1.8.0, build f3628c7" ]]; then
   echo -e "\t\e[1;4;28;41m Docker Compose 1.8.0 \e[0m"
 else
-  echo -e "\t\e[1;4;28;41m $docker_compose_version \e[0m"
+  echo -e "\t\e[1;4;28;41m $docker_compose_version \e[0m \t\e[1;4;31;40m* UPDATED *\e[0m"
 fi
 printf "\n"
 
 sleep 0.5
-
+################################################################################
 echo NodeJS version:
 printf "\n"
-echo -e "\t\e[1;4;28;41m NodeJS $nodejs_version \e[0m"
+if [[ $nodejs_version = "v4.2.6" ]]; then
+  echo -e "\t\e[1;4;28;41m NodeJS v4.2.6  \e[0m"
+else
+  echo -e "\t\e[1;4;28;41m NodeJS $nodejs_version \e[0m \t\e[1;4;31;40m* UPDATED *\e[0m"
+fi
 printf "\n"
 
 sleep 0.5
-
+################################################################################
 echo Bundler version:
 printf "\n"
 if [[ $bundler_version = "Bundler version 1.12.5" ]]; then
   echo -e "\t\e[1;4;28;41m Bundler 1.12.5 \e[0m"
 else
-  echo -e "\t\e[1;4;28;41m $bundler_version \e[0m"
+  echo -e "\t\e[1;4;28;41m $bundler_version \e[0m \t\e[1;4;31;40m* UPDATED *\e[0m"
 fi
 printf "\n"
 
 sleep 0.5
-
+################################################################################
 echo Ruby version:
 printf "\n"
 
 if [[ $ruby_version = "ruby 2.3.1p112 (2016-04-26 revision 54768) [x86_64-linux]" ]]; then
   echo -e "\t\e[1;4;28;41m Ruby 2.3.1p112 \e[0m"
 else
-  echo -e "\t\e[1;4;28;41m $ruby_version \e[0m"
+  echo -e "\t\e[1;4;28;41m $ruby_version \e[0m \t\e[1;4;31;40m* UPDATED *\e[0m"
 fi
 printf "\n"
 
 sleep 0.5
-
+################################################################################
 echo Rails version:
 printf "\n"
-echo -e "\t\e[1;4;28;41m $rails_version \e[0m"
+if [[ $rails_version = "Rails 5.0.0" ]]; then
+  echo -e "\t\e[1;4;28;41m Rails 5.0.0 \e[0m"
+else
+  echo -e "\t\e[1;4;28;41m $rails_version \e[0m \t\e[1;4;31;40m* UPDATED *\e[0m"
+fi
 printf "\n\n"
 
-cat message/done.txt
+sleep 1
+
+cat ~/Documents/ecs_teste/message/done.txt
 
 printf "\n"
