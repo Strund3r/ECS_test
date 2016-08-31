c6b8d2a51e20d2323309161c14ad92a4d6cfa8a9
Strund3r <franzwagner.str@gmail.com>
2016-08-31 09:12:49 -0300

    Updated run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,16 @@
 
 if [ -d "~/myapp" ]; then
   ./RVM_install_server.sh
-  rm -f ~/myapp/tmp/pids/server.pid
+  rm -f /myapp/tmp/pids/server.pid
   printf "\n\n"
   cat ~/myapp/message/server.txt
   printf "\n\n"
 else
   ./RVM_install_machine.sh
   rm -f ~/Documents/ecs_teste/tmp/pids/server.pid
-  rm -f ~/myapp/tmp/pids/server.pid
+  rm -f /myapp/tmp/pids/server.pid
   printf "\n\n"
-  cat /message/localhost.txt
+  cat ~/message/localhost.txt
   printf "\n\n"
 fi
 
