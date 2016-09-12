59e373594e74b176705d59014af2de8aef55336a
Strund3r <franzwagner.str@gmail.com>
2016-09-12 15:02:33 -0300

    UPDATED APP

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
-FROM ruby:2.3.1-onbuild
+FROM ubuntu:16.04
 
 MAINTAINER Franzwagner Ternus <franzwagner.str@gmail.com>
 
-RUN apt-get update -qq && apt-get install -y apt-utils build-essential libpq-dev redis-server libgmp3-dev
+RUN apt-get update -qq && apt-get install -y apt-utils build-essential libpq-dev redis-server libgmp3-dev libsqlite3-dev sqlite3 ruby rubygems ruby-dev zlib1g-dev
 
-COPY APPLICATION /var/www
-WORKDIR /var/www
+COPY APPLICATION /app
+WORKDIR /app
 RUN /usr/bin/gem install bundler
 RUN /usr/local/bin/bundle install
+CMD ["./check_versions.sh"]
 
-ADD run.sh /usr/local/bin/
+ENV RAILS_ENV=development
+RUN bundle exec rake assets:precompile --trace
+COPY run.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/run.sh
 CMD ["/usr/local/bin/run.sh"]
--- run.sh
+++ run.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-if [ -d "/var/www/" ]; then
-  sleep 15
-  cd /var/www
-  ./RVM_install_server.sh
-  rm -f /var/www/tmp/pids/server.pid
-  printf "\n\n"
-  cat /var/www/message/server.txt
-  printf "\n\n"
-else
+if [ -d "/ecs_teste" ]; then
+  cd /ecs_teste/APPLICATION
   ./RVM_install_machine.sh
   rm -f /ecs_teste/APPLICATION/tmp/pids/server.pid
-  rm -f /var/www/tmp/pids/server.pid
+  rm -f /app/tmp/pids/server.pid
   printf "\n\n"
   cat /ecs_teste/APPLICATION/message/localhost.txt
   printf "\n\n"
+  bundle exec rails server -b '0.0.0.0'
+else
+  sleep 15
+  cd /app
+  rm -f /app/tmp/pids/server.pid
+  printf "\n\n"
+  cat /app/message/server.txt
+  printf "\n\n"
+  rake db:create
+  rake db:migrate
+  bundle exec rails server -b '0.0.0.0'
 fi
-
-bundle exec rails server -b '0.0.0.0'
